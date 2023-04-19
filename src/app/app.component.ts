0dda5789b177e886fe30b3ceb14c91584da5cde8
Sohaib Kamran <sohaib.kamran.37@gmail.com>
2023-04-19 19:58:15 +0000

    crd complete update required

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 interface Todo{
+  id: number
   description:string
   done:boolean
 }
@@ -12,12 +13,57 @@ interface Todo{
 export class AppComponent {
   title = 'todoApp';
   todos:Todo[]=[]
-  description:FormControl = new FormControl('',Validators.required)
+  todoForm!: FormGroup;
+  type: string='todos';
+  constructor(private fb:FormBuilder){
+    this.formInit()
+  }
+  formInit(){
+    this.todoForm = this.fb.group({
+      description:['my 1st todo',Validators.required]
+    })
+  }
+  get description(){
+    return this.todoForm?.get('description')
+  }
   saveTodo(){
     let todo = {
-      description:this.description.value,
+      id: this.todos.length+1,
+      description:this.description?.value,
       done:false
     }
     this.todos.push(todo)
+    localStorage.setItem('todos',JSON.stringify(this.todos))
+  }
+  deleteTodo(todo:Todo,i:number){
+    // this.todos = this.todos.filter((data,j)=>j!=i)
+    // this.todos = this.todos.filter((data,j)=>data.description!=todo.description)
+    this.todos = this.todos.filter((data,j)=>data.id!=todo.id)
+    // let newArray:Todo[]=[]
+    // for (let i = 0; i < this.todos.length; i++) {
+    //   debugger
+    //   if(this.todos[i].id!=todo.id)
+    //     newArray.push(this.todos[i])
+    // }
+    // this.todos = newArray
+    localStorage.setItem('todos',JSON.stringify(this.todos))
+  }
+  done(i:number){
+    this.todos[i].done = !this.todos[i].done
+    localStorage.setItem('todos',JSON.stringify(this.todos))
+    // this.filter(this.type)
+  }
+  filter(value:string){
+    this.type = value;
+    if(value=="rem"){
+      this.todos = JSON.parse(localStorage.getItem('todos')!)
+      this.todos = this.todos.filter(data=>data.done==false)
+    }else if(value=="com"){
+      this.todos = JSON.parse(localStorage.getItem('todos')!)
+      this.todos = this.todos.filter(data=>data.done==true)
+    }else{
+      this.todos = JSON.parse(localStorage.getItem('todos')!)
+      // this.todos = this.todos.filter(data=>(data.done==false || data.done==true))
+    }
   }
 }
