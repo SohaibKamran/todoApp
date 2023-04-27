fe1ece44cdca728a769d53d228d69fe18f7053b4
Sohaib Kamran <sohaib.kamran.37@gmail.com>
2023-04-27 18:54:44 +0000

    setup firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -13,9 +19,16 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    firebase: {
+      projectId: 'dummy-cdfdf',
+      appId: 'xxxxxxxxxxxxxxxx',
+      storageBucket: 'xxxxxxxxxxxx',
+      apiKey: 'xxxxxxxxxxxxxxxxx',
+      authDomain: 'xxxxxxxxxxxxxxxxxxxx',
+      messagingSenderId: 'xxxxxxxxxxxxx',
+      measurementId: 'xxxxxxxxxxxxxx',
+    },
+    production:false
+  }
